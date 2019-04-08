9de66d5083e3092d9bd48e5b08602c53c4bda162
Jason Brando <jasonbrandoo@gmail.com>
2019-04-08 16:51:36 +0700

    16:51 Apri 2019 add sales target

---
+++ app/Model/SalesTarget.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SalesTarget extends Model
+{
+    //
+    protected $table = 'sales_target';
+    protected $guarded = [];
+}
